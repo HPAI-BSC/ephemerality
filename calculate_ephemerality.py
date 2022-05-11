75b13885e7899a520bae50fe239847974a6b0254
Dmitry <dmitry.gnatyshak@bsc.es>
2022-05-11 16:35:54 +0200

    Change input format
    Related to issue #33

--- calculate_ephemerality.py
+++ calculate_ephemerality.py
@@ -1,11 +1,15 @@
 import sys
 import json
+import numpy as np
 from src import compute_ephemerality_measures
 
 
 if __name__ == '__main__':
-    frequency_vector = json.loads(sys.argv[1])
+    frequency_vector_file = sys.argv[1]
     output_file = sys.argv[2]
+
+    with open(frequency_vector_file, 'r') as f:
+        frequency_vector = np.array(f.read().split(','), dtype=float)
 
     ephemeralities = compute_ephemerality_measures(frequency_vector=frequency_vector,
                                                    threshold=0.8)
